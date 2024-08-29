824f78381746a6eb3a6ee640014d1c4c957f28d1
ChrisHashing <chriswindow466@gmail.com>
2024-08-29 11:17:15 +0530

    Removed navs

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,4 @@
 
 export { default as ChainSelector } from './components/chain-selector/LegacyChainSelector';
 export { default as SearchBar } from './components/search-bar/LegacySearchBar';
-export { default as SideNavbar } from './components/side-navbar/LegacySideNavBar';
 export { default as Tables } from './components/tables/LegacyTables';
-export { default as TopNavbar } from './components/top-navbar/LegacyTopNavBar';
